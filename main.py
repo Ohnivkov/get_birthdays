bc526b9d7f0f9978ceb3f398e46f5765e6b93f1e
Ohnivkov <136961385+Ohnivkov@users.noreply.github.com>
2023-08-28 11:02:54 +0300

    new_program

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ from datetime import datetime,timedelta
 
 weekdays_dict={'Monday':[],'Tuesday':[],'Wednesday':[],'Thursday':[],'Friday':[]}
 
-weakdays_list = ['Monday','Tuesday','Wednesday','Thursday','Friday']
-
 current_datetime = datetime.now().date()
 interval = current_datetime + timedelta(weeks=1)
 
 def get_birthdays_per_week(users):
-    for i in users:
-        if i['birthday']>=current_datetime and i['birthday']<interval:
-            weekdays_dict[get_day(i['birthday'])].append(i['name'])
+    for user in users:
+        user['birthday']=datetime(year=current_datetime.year,month=user['birthday'].month,day=user['birthday'].day).date()
+        if user['birthday']>=current_datetime and user['birthday']<=interval:
+            weekdays_dict[get_day(user['birthday'])].append(user['name'])
 
     for i in weekdays_dict:
-        print(f'{i}:',','.join(weekdays_dict[i]))
+        if weekdays_dict[i]:
+            print(f'{i}:',','.join(weekdays_dict[i]))
 
 def get_day(date):
-    if date.weekday()==5 or date.weekday()==6:
-        return weakdays_list[0]
+    if date.weekday() in (5,6):
+        return 'Monday'
     else:
-        return weakdays_list[date.weekday()]
+        return date.strftime('%A')
